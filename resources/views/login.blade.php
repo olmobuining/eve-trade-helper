0c3f9b757a025d9a5c6f1c2b3b72efcfd6cae8ee
olmobuining <olmobuining@gmail.com>
2017-07-08 10:59:43 +0200

    Correct classed for an alert box

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,7 +16,7 @@
                     </div>
                     <div class="panel-body">
                         @if (Session::has('flash_message'))
-                            <div class="alert-block ale alert-error alert">
+                            <div class="alert alert-danger">
                                 {{ Session::get('flash_message') }}
                             </div>
                         @endif
